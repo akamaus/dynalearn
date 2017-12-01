506c15686bc3acd1f84f19ed9858fff23ba37e1d
Dmitry Vyal <dmitryvyal@gmail.com>
2017-12-01 09:38:36 +0300

    forgotten file

---
+++ video_utils.py
@@ -0,0 +1,48 @@
+import imageio as ii
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def concat_images(img1, img2):
+    padder = np.zeros([img1.shape[0], 20, 3])
+    return np.concatenate([img1, padder, img2], axis=1)
+
+
+def video_reader(path, num_frames = None, rotate=False):
+    reader = ii.get_reader(path)
+    for k, img in enumerate(reader):
+        #print("reading {} of {}".format(k, num_frames))
+        if rotate:
+            img = np.transpose(img, axes=[1, 0, 2])
+            img = np.fliplr(img)
+
+        if num_frames is None or k < num_frames:
+            yield img
+        else:
+            raise StopIteration
+
+
+class VideoWriter:
+    def __init__(self, path=None, show=False):
+        if path:
+            self.writer = ii.get_writer(path, fps=30)
+        else:
+            self.writer = None
+        self.show = show
+        if show:
+            plt.ion()
+        self.im = None
+
+    def consume(self, img):
+        if self.writer:
+            self.writer.append_data(img)
+        if self.show:
+            if self.im is None:
+                self.im = plt.imshow(img)
+            else:
+                self.im.set_data(img)
+            plt.pause(0.01)
+
+    def close(self):
+        if self.writer:
+            self.writer.close()
