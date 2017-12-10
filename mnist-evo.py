c5c451b2a14bab92fc89cde8c4f90d25453be5f2
Dmitry Vyal <dmitryvyal@gmail.com>
2017-12-10 15:41:50 +0300

    try to learn the digit cycle

--- mnist-evo.py
+++ mnist-evo.py
@@ -1,62 +1,98 @@
+import numpy as np
+
 import torch as T
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision as TV
+
 from torch.autograd import Variable
 
 import video_utils as VU
 
 mnist = TV.datasets.MNIST('MNIST_DATA', download=True, transform=TV.transforms.Compose([TV.transforms.ToTensor()]))
+num_digits = 10
 
 samples = []
-for i in range(10):
+for i in range(num_digits):
     k = 0
     for img, lbl in mnist:
         if lbl == i:
             print('found digit %d at %d' % (i, k))
-            samples.append(img)
+            samples.append(img.cuda())
             break
         k += 1
 
-dataset = T.utils.data.TensorDataset(T.stack(samples).cuda(), T.stack(samples[1:] + [samples[0]]).cuda())
-loader = T.utils.data.DataLoader(dataset, batch_size=4)
+targets = samples[1:] + [samples[0]]
+
+dataset = T.utils.data.TensorDataset(T.stack(samples), T.stack(targets))
+loader = T.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
 
 fs = 8
 
 class LinearModel(nn.Module):
-    def __init__(self):
+    def __init__(self, l2):
         super(LinearModel, self).__init__()
-        self.lin1 = nn.Linear(28*28, 28*28)
+        self.lin1 = nn.Linear(28*28, l2)
+        self.lin2 = nn.Linear(l2, 28 * 28)
 
     def forward(self, inp):
-        s = inp.size()
-        inp = inp.view(s[0], -1)
-        x = self.lin1(inp)
-        x = x.view(s)
+        sz = inp.size()
+        inp = inp.view(sz[0], -1)
+        x = F.relu(self.lin1(inp))
+        x = self.lin2(x)
+        return x.view(sz)
+
+class ConvModel(nn.Module):
+    def __init__(self):
+        super(ConvModel, self).__init__()
+        self.pos_pad = nn.ZeroPad2d((0, 1, 0, 1))
+        self.neg_pad = nn.ZeroPad2d((0, -1, 0, -1))
+        self.r = nn.ReLU()
+
+        self.conv1 = nn.Conv2d(1, 8, 3, 2)
+        self.conv2 = nn.Conv2d(8, 16, 3, 2)
+        self.conv3 = nn.Conv2d(16, 32, 3, 2)
+
+        s = 3
+        self.lin1 = nn.Linear(32*s*s, 32*s*s)
+
+        self.tconv3 = nn.ConvTranspose2d(32, 16, 3, 2)
+        self.tconv2 = nn.ConvTranspose2d(16, 8, 3, 2)
+        self.tconv1 = nn.ConvTranspose2d(8, 1, 3, 2)
+
+    def forward(self, x):
+        x = self.r(self.conv1(self.pos_pad(x)))
+        x = self.r(self.conv2(self.pos_pad(x)))
+        x = self.r(self.conv3(x))
+
+        s = x.size()
+        x = self.r(self.lin1(x.view([s[0], -1]))).view(s)
+
+        x = self.r(self.tconv3(x))
+        x = self.r(self.neg_pad(self.tconv2(x)))
+        x = self.r(self.neg_pad(self.tconv1(x)))
         return x
 
-model = LinearModel().cuda()
 
-# model = nn.Sequential(nn.ZeroPad2d((0, 1, 0, 1)), nn.Conv2d(1, 8, 3, 2), nn.ReLU(),
-#                       nn.ZeroPad2d((0, 1, 0, 1)), nn.Conv2d(8, 16, 3, 2), nn.ReLU(),
-#                       nn.Conv2d(16, 32, 3, 2), nn.ReLU(),
-#                       nn.ConvTranspose2d(32, 16, 3, 2), nn.ReLU(),
-#                       nn.ConvTranspose2d(16, 8, 3, 2), nn.ZeroPad2d((0, -1, 0, -1)), nn.ReLU(),
-#                       nn.ConvTranspose2d(8, 1, 3, 2), nn.ZeroPad2d((0, -1, 0, -1)), nn.ReLU()).cuda()
+model = LinearModel(l2=100).cuda()
+#model = ConvModel().cuda()
 
 
 for m in model.modules():
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
         nn.init.xavier_uniform(m.weight.data)
 
-opt = T.optim.Adam(params=model.parameters())
+opt = T.optim.Adam(params=model.parameters(), lr=0.0001)
 
 vu = VU.VideoWriter('tst.mp4', show=True)
 
+smpl = T.unsqueeze(samples[0], 0)
 for ep in range(1000):
-
-    for inp, tgt in loader:
-        inp = Variable(inp)
-        tgt = Variable(tgt)
+    outs = []
+    for si in range(num_digits):
+#        smpl = T.unsqueeze(samples[si], 0)
+        inp = Variable(smpl)
+        tgt = Variable(T.unsqueeze(targets[si], 0))
 
         opt.zero_grad()
         out = model(inp)
@@ -64,10 +100,14 @@ for ep in range(1000):
         loss.backward()
         opt.step()
 
-    if ep % 10 == 0:
-        print('ep %d; loss %f' % (ep, loss.cpu().data))
-        t_inp, t_tgt = dataset[:]
-        s = t_inp.size()
-        t_out = model(Variable(t_inp)).data
-        frame = T.cat([t_out, t_tgt, T.zeros_like(t_out)], dim=1).cpu().permute(1,2,0,3).contiguous().view([3, s[2], s[3]*s[0]])
-        vu.consume(frame.numpy().transpose(1,2,0))
+        outs.append(out.data.cpu()[0])
+        smpl = out.data.clamp(0,1)
+
+    print('ep %d; loss %f' % (ep, loss.cpu().data))
+    t_outs = T.stack(outs, 0)
+    t_tgts = T.stack(targets).cpu()
+
+    sz = samples[0].size()
+    frame = T.cat([t_outs, t_tgts, T.zeros_like(t_outs)], dim=1).permute(1,2,0,3).contiguous().view([3, sz[1], sz[2] * len(outs)])
+    n_frame = (frame.permute(1,2,0).clamp(min=0, max=1) * 255).numpy().astype(np.uint8)
+    vu.consume(n_frame)
