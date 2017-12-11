58836666bcd3fbb55649af56a09054bdc9e92e3e
Dmitry Vyal <dmitryvyal@gmail.com>
2017-12-11 09:04:35 +0300

    learning using whole batch of random-initialized states

--- mnist-evo.py
+++ mnist-evo.py
@@ -55,8 +55,9 @@ class ConvModel(nn.Module):
         self.conv2 = nn.Conv2d(8, 16, 3, 2)
         self.conv3 = nn.Conv2d(16, 32, 3, 2)
 
-        s = 7
-        self.lin1 = nn.Linear(16*s*s, 16*s*s)
+        f = 32
+        s = 3
+        self.lin1 = nn.Linear(f*s*s, f*s*s)
 
         self.tconv3 = nn.ConvTranspose2d(32, 16, 3, 2)
         self.tconv2 = nn.ConvTranspose2d(16, 8, 3, 2)
@@ -65,67 +66,83 @@ class ConvModel(nn.Module):
     def forward(self, x):
         x = self.r(self.conv1(self.pos_pad(x)))
         x = self.r(self.conv2(self.pos_pad(x)))
-#        x = self.r(self.conv3(x))
+        x = self.r(self.conv3(x))
 
         s = x.size()
         x = self.r(self.lin1(x.view([s[0], -1]))).view(s)
 
-#        x = self.r(self.tconv3(x))
+        x = self.r(self.tconv3(x))
         x = self.r(self.neg_pad(self.tconv2(x)))
         x = self.r(self.neg_pad(self.tconv1(x)))
         return x
 
 
-model = LinearModel(l2=100).cuda()
-#model = ConvModel().cuda()
+#model = LinearModel(l2=100).cuda()
+model = ConvModel().cuda()
 
 
 for m in model.modules():
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
         nn.init.xavier_uniform(m.weight.data)
 
-opt = T.optim.Adam(params=model.parameters(), lr=0.01)
+opt = T.optim.Adam(params=model.parameters(), lr=0.0001)
 #opt = T.optim.SGD(params=model.parameters(), lr=0.0001, momentum=0.8, nesterov=True)
 
 vu = VU.VideoWriter('tst.mp4', show=True)
 losses = []
 losses_std = []
 
-num_epochs = 1000
+num_epochs = 10000
+batch_size = 32
 
-smpl = T.unsqueeze(samples[0], 0)
+sis = []
+for nb in range(batch_size):
+    sis.append(np.random.randint(num_digits))
+sis = np.array(sis)
+sis[0] = 0
+
+def build_state(arr, sis):
+    return T.stack(list(map(lambda si: arr[si], sis)), dim=0)
+
+
+smpls = build_state(samples, sis)
+
 for ep in range(num_epochs):
-    outs = []
+    loop_outs = []
+    loop_tgts = []
     loop_losses = []
-    for si in range(num_digits):
+    for i in range(num_digits):
         if ep < num_epochs-600:
-            smpl = T.unsqueeze(samples[si], 0)
+            smpls = build_state(samples, sis)
 
-        smpl += (T.rand(smpl.size())*0.001 - 0.0005).cuda()
-        inp = Variable(smpl)
-        tgt = Variable(T.unsqueeze(targets[si], 0))
+        smpls += (T.rand(smpls.size())*0.001 - 0.0005).cuda()
+        inps = Variable(smpls)
+        tgts = Variable(build_state(targets, sis))
 
         opt.zero_grad()
-        out = model(inp)
-        loss = T.mean((out - tgt)**2)
+        outs = model(inps)
+        loss = T.mean((outs - tgts)**2)
         if ep < num_epochs - 300:
             loss.backward()
             opt.step()
 
         loop_losses.append(loss.cpu().data)
-        outs.append(out.data.cpu()[0])
-        smpl = out.data.clamp(0,1)
+        loop_outs.append(outs[0].data.cpu())
+        loop_tgts.append(tgts[0].data.cpu())
+        smpls = outs.data.clamp(0, 1)
+        sis += 1
+        sis %= num_digits
 
     losses.append(np.mean(loop_losses))
     losses_std.append(np.std(loop_losses))
 
-    if ep % 5 == 0:
+    if ep % 10 == 0:
         print('ep %d; loss %f +- %f' % (ep, losses[-1], losses_std[-1]))
-        t_outs = T.stack(outs, 0)
-        t_tgts = T.stack(targets).cpu()
+        t_outs = T.stack(loop_outs)
+        t_tgts = T.stack(loop_tgts)
 
         sz = samples[0].size()
-        frame = T.cat([t_outs, t_tgts, T.zeros_like(t_outs)], dim=1).permute(1,2,0,3).contiguous().view([3, sz[1], sz[2] * len(outs)])
+        frame = T.cat([t_outs, t_tgts, T.zeros_like(t_outs)], dim=1).permute(1,2,0,3).contiguous().view([3, sz[1], sz[2] * len(loop_outs)])
         n_frame = (frame.permute(1,2,0).clamp(min=0, max=1) * 255).numpy().astype(np.uint8)
         vu.consume(n_frame)
 
