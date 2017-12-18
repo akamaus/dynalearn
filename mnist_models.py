5d78c0e0ece954f3d18d054eb36d30643fd22e9c
Dmitry Vyal <dmitryvyal@gmail.com>
2017-12-18 21:53:17 +0300

    old models moved to models.py

--- mnist_models.py
+++ mnist_models.py
@@ -103,3 +103,74 @@ class Discriminator(PersistentModule):
         out = out.view(-1, 4*4*4*DIM)
         out = self.output(out)
         return out.view(-1)
+
+# MINE old models
+
+class LinearModel(nn.Module):
+    def __init__(self, l2):
+        super(LinearModel, self).__init__()
+        self.lin1 = nn.Linear(28*28, l2)
+        self.lin2 = nn.Linear(l2, 28 * 28)
+
+    def forward(self, inp):
+        sz = inp.size()
+        inp = inp.view(sz[0], -1)
+        x = F.relu(self.lin1(inp))
+        x = self.lin2(x)
+        return x.view(sz)
+
+
+class ConvDisc(PersistentModule):
+    def __init__(self, name):
+        super(ConvDisc, self).__init__('ConvDisc_' + name)
+        self.pos_pad = nn.ZeroPad2d((0, 1, 0, 1))
+        self.neg_pad = nn.ZeroPad2d((0, -1, 0, -1))
+        self.r = nn.ReLU()
+
+        self.conv1 = nn.Conv2d(1, 16, 3, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, 2)
+        self.conv3 = nn.Conv2d(32, 64, 3, 2)
+
+        f = 64
+        s = 3
+        self.lin1 = nn.Linear(f*s*s, 1)
+
+    def forward(self, x):
+        x = self.r(self.conv1(self.pos_pad(x)))
+        x = self.r(self.conv2(self.pos_pad(x)))
+        x = self.r(self.conv3(x))
+
+        s = x.size()
+        x = self.lin1(x.view([s[0], -1]))
+        return x
+
+
+class ConvGen(PersistentModule):
+    def __init__(self, name):
+        super(ConvGen, self).__init__('ConvGen_' + name)
+
+        self.f = 64
+        self.s = 3
+        self.inp_size = self.f * self.s ** 2
+
+        self.r = nn.ReLU()
+        self.pos_pad = nn.ZeroPad2d((0, 1, 0, 1))
+        self.neg_pad = nn.ZeroPad2d((0, -1, 0, -1))
+
+        #self.lin1 = nn.Linear(inp_size, self.f * self.s ** 2)
+
+        self.tconv3 = nn.ConvTranspose2d(64, 32, 3, 2)
+        self.tconv2 = nn.ConvTranspose2d(32, 16, 3, 2)
+        self.tconv1 = nn.ConvTranspose2d(16, 1, 3, 2)
+
+    def forward(self, x):
+        sz = x.size()
+        assert sz[1] == self.inp_size
+
+        x = x.view((sz[0], self.f, self.s, self.s))
+
+        x = self.r(self.tconv3(x))
+        x = self.r(self.neg_pad(self.tconv2(x)))
+        x = self.neg_pad(self.tconv1(x))
+#        x = F.sigmoid(x)
+        return x
